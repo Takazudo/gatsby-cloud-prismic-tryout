a74bb05172806b45ea7935e545f25de26ca96d4c
Takeshi Takatsudo <takazudo@gmail.com>
2021-06-09 16:46:26 +0900

    setup gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>hoge</h1>
+      <p>The quick brown fox jumps over the lazy dog. </p>
+    </main>
+  );
+};
+
+export default IndexPage;
